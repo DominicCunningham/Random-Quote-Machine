721b3dad15f7f7f86c97ec4f2fe1757af51d751c
Dominic Cunningham <dominic@cunninghamitup.com>
2018-01-30 17:43:02 -0500

    random number is now = to quote no author as of now

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
-function randomQuote() {
-  var quote = ["Once We Accept Our Limits, We Go Past Them", "Whatever the mind of man can conceive and believe, it can achieve.", "Strive not to be a success, but rather to be of value."];
-  var author = ["Albert Einstein", "Napoleon Hill", "Albert Einstein"];
-  var randomNumber = Math.floor(Math.random() * 2 - 0 + 1)) + 0;
-  var RQuote = quote[randomNumber];
-  var RAuthor = author[randomNumber];
-  console.log(RQuote);
-  console.log(RAuthor);
-}
+function randomNumber() {
+    var randomNumber = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+    console.log(randomNumber);
+    var quote = [
+      'Once We Accept Our Limits, We Go Past Them', 'Strive not to be a success, but rather to be of value.',
+      ' I attribute my success to this: I never gave or took any excuse.', 'I attribute my success to this: I never gave or took any excuse.',
+      ' You miss 100% of the shots you don’t take. ', 'Every strike brings me closer to the next home run.',
+      'We become what we think about.', 'Eighty percent of success is showing up.',
+      'Either you run the day, or the day runs you. ', 'The best revenge is massive success.'
+    ];
+    document.getElementById('RandomQuote').innerHTML = quote[randomNumber];
+  }
