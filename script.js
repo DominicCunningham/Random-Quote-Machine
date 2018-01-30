f015ca37ced8f10504f99f2ecbcb5756f7d243d2
Dominic Cunningham <dominic@cunninghamitup.com>
2018-01-30 15:48:06 -0500

    random number now connected to a quote + author

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function randomNumber() {
-  var number = Math.floor(Math.random() * (99 - 0 + 1)) + 0;
-  console.log(number)
+
+function randomQuote() {
+  var quote = ["Once We Accept Our Limits, We Go Past Them", "Whatever the mind of man can conceive and believe, it can achieve.", "Strive not to be a success, but rather to be of value."];
+  var author = ["Albert Einstein", "Napoleon Hill", "Albert Einstein"];
+  var randomNumber = Math.floor(Math.random() * 2 - 0 + 1)) + 0;
+  var RQuote = quote[randomNumber];
+  var RAuthor = author[randomNumber];
+  console.log(RQuote);
+  console.log(RAuthor);
 }
